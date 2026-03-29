c82c6a7729f4b3b5d39f10695030a5d1c2ac295c
wschub <dev.wschub@gmail.com>
2026-03-28 20:01:08 -0500

    fix migrations

--- check_dupes.sql
+++
@@ -1,1 +0,0 @@
-SELECT firebase_uid, COUNT(*) FROM "User" GROUP BY firebase_uid HAVING COUNT(*) > 1;
--- fix_migration.sql
+++
@@ -1,1 +0,0 @@
-DELETE FROM "_prisma_migrations" WHERE migration_name = '20260313160712_4_4_modify_column_order_x_section_order';
